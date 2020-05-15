1e88fb946ce872046127ab0cbc6e6953731e79ea
Prashant <prashantwanedkar95@gmail.com>
2020-05-15 22:18:56 +0530

    Models Changes

---
+++ GIFTechnologies/UserDetails/Models/ParentStudent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserDetails.Models
+{
+    public class ParentStudent
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public int StudentId { get; set; }
+    }
+}
---
+++ GIFTechnologies/UserDetails/Models/StudentDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserDetails.Models
+{
+    public class StudentDetails
+    {
+        public int StudentId { get; set; }
+        public string StudentFirstName { get; set; }
+        public string StudentLastName { get; set; }
+        public int ParentId { get; set; }
+        public string ParentFirstName { get; set; }
+        public string ParentLastName { get; set; }
+        public string ParentEmail { get; set; }
+        public string ParentMobile { get; set; }
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public bool Active { get; set; }
+
+        public List<SchoolClass> SchoolClass { get; set; }
+
+    }
+}
