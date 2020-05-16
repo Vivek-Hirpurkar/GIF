55397d3fa79a8d15b190ff49504bc7f7b6e99d85
Prashant <prashantwanedkar95@gmail.com>
2020-05-16 09:28:49 +0530

    Changes

---
+++ GIFTechnologies/UserDetails/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserDetails.Models;
+
+namespace UserDetails.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult GetAllStudents()
+        {
+            StudentDBHelper db = new StudentDBHelper();
+            List<StudentDetails> students = db.GetAllStudents();
+            return View(students);
+        }
+    }
+}
